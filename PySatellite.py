2822579b321cda7a2863c6a97509ba3e303eb6c4
Kristof Ostir <krostir@gmail.com>
2017-02-07 22:04:21 +0100

    Initial package
    Initial package release
    Read SP3 in dataframe

--- PySatellite.py
+++ PySatellite.py
@@ -4,7 +4,8 @@
 # Imports
 import pandas as pd
 import datetime
-
+import numpy as np
+from io import StringIO
 
 # Parameters
 # Difference beetween UTM in and GPS time
@@ -64,3 +65,75 @@ def time_gps2utc(dt):
     time_diff = int(utm_gps_diff[:dt]["GPS_UTC"][-1])
     time_utm = dt + datetime.timedelta(seconds=-time_diff)
     return time_utm
+
+# Download SP3 file
+def sp3_download(dt, data_folder):
+    """
+    sp3_download: Download SP3 file closest to specified date
+
+    Args:
+        dt: GPS date and time, if missing current time is used
+        data_folder: Location of downloaded file
+
+    Returns:
+        res: True on success
+    """
+
+    return res
+
+# Read SP3 file
+def read_sp3(sp3_fn):
+    """
+    read_sp3: Reads SP3 file to dataframe
+
+    Args:
+        sp3_fn: file name
+
+    Returns:
+        res: dataframe
+    """
+
+    # Dataframe column names
+    df_cols = ["dt_y", "dt_m", "dt_d", "dt_hour", "dt_min", "dt_secfra",
+               "vehicle", "coor_x", "coor_y", "coor_z", "clock",
+               "std_x", "std_y", "std_z", "std_c"]
+    # Open file and prepare IO string
+    try:
+        spf_in = open(sp3_fn)
+    except:
+        return False
+    spf_s = StringIO()
+    spf_in_lines = spf_in.readlines()[22:]
+
+    # Read SP3 and extract orbit part
+    # Line with GPS time is followed by satellite data
+    cur_time = None
+    for ln in spf_in_lines:
+        if not ln.strip():
+            continue
+        if ln.startswith('*  '):
+            cur_time = ",".join(ln.split()[1:])
+            continue
+        if ln.startswith('EOF'):
+            continue
+        if cur_time is None:
+            print('NO time found')
+            sys.exit(1)
+        spf_s.write(cur_time + "," + ",".join(ln.split()[:9]) + "\n")
+    spf_s.seek(0)
+
+    # Create initial dataframe
+    sp3_df = pd.read_csv(spf_s, header=None,
+                         names=df_cols)
+    sp3_df["dt_sec"] = sp3_df["dt_secfra"].astype(int)
+    sp3_df["dt_mic"] = ((sp3_df["dt_secfra"] - sp3_df["dt_sec"]) * 1e6).astype(int)
+    sp3_df['date_time'] = \
+        sp3_df[['dt_y', 'dt_m', 'dt_d',
+                "dt_hour", "dt_min", "dt_sec",
+                "dt_mic"]].apply\
+            (lambda s: datetime.datetime(*s), axis=1)
+    sp3_df.drop(['dt_y', 'dt_m', 'dt_d',
+                "dt_hour", "dt_min", "dt_sec",
+                "dt_mic", "dt_secfra"], axis=1, inplace=True)
+    # sp3_df.set_index('date_time')
+    return sp3_df
