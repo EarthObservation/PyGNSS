88fd02c00ea8b36f645fa961c563f63e0ae3fcbd
Kristof Ostir <krostir@gmail.com>
2017-02-10 14:55:50 +0100

    Test download and extract
    Download SP3, reformat and write to CSV

---
+++ Test/PyS_download_reproject.py
@@ -0,0 +1,32 @@
+# PySatellite test
+#
+# Download SP3, reproject and extract by satellite
+#
+# Kristof Ostir, 2017-02-10
+# University of Ljubljana, Faculty of Civil and Geodetic Engineering
+# (c) 2017
+
+# Imports
+import PySatellite as pys
+
+# Parameters
+data_folder = "D:/Kristof/Python/PySatellite/Data/"
+data_file = "D:/Kristof/Python/PySatellite/Data/igu19355_06.sp3"
+
+# Read current SP3 for current time
+# sp3_fn = pys.download_sp3(data_folder="D:/Kristof/Python/PySatellite/Data/")
+
+# Read data to dataframe
+sp3_df = pys.read_sp3(data_file)
+
+# Reproject
+sp3_df_lla = pys.convert_ecef2lla(sp3_df)
+
+# Display data
+print(sp3_df.head())
+print(sp3_df_lla.head())
+
+# Positions to file
+sat_pos = sp3_df_lla[["coor_x", "coor_y", "coor_z",	"lat", "long", "alt"]]
+for name, group in sat_pos.groupby(level=[0]):
+    group.to_csv("%s_sp3_positions.csv" % name)
