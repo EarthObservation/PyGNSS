b726e83cc81c514164a94465b8173d832a1bf63b
Kristof Ostir <krostir@gmail.com>
2017-02-10 14:55:13 +0100

    Update library
    Update library
    Functions to download, read, reproject, reformat

--- PySatellite.py
+++ PySatellite.py
@@ -1,11 +1,21 @@
-# PySatellite
-# Analysis of GNSS data
+# Analysis an visualisation of GNSS precise orbits
+#
+# Functions for downloading , time conversion, reprojection and visualisation of precise GNSS orbits
+#
+# Krištof Oštir and Polona Pavličič Prešeren
+# University of Ljubljana, Faculty of Civil and Geodetic Engineering
+# (c) 2017
+
 
 # Imports
 import pandas as pd
 import datetime
 import numpy as np
 from io import StringIO
+import pyproj
+from ftplib import FTP
+import subprocess
+import os
 
 # Parameters
 # Difference beetween UTM in and GPS time
@@ -31,6 +41,19 @@ utm_gps_diff = pd.DataFrame([("1980-01-01", 0),
                             columns=["From", "GPS_UTC"])
 utm_gps_diff["From"]  = pd.to_datetime(utm_gps_diff['From'])
 utm_gps_diff = utm_gps_diff.set_index("From")
+# Projections
+ecef = pyproj.Proj(proj='geocent', ellps='WGS84', datum='WGS84')
+lla = pyproj.Proj(proj='latlong', ellps='WGS84', datum='WGS84')
+# FTP server
+ftp_address = "igs.ensg.ign.fr"
+ftp_data_root = "/pub/igs/products/"
+fn_data_root = "D:/Kristof/Python/PySatellite/Data/"
+# Unzip tool, complete path or command, 7-ZIP handles .Z on Windows
+gunzip = "7z"
+# GPS epoch, time measurements
+secWeek = 604800 # seconds in week
+secDay = 86400 # seconds in day
+gpsStart = datetime.datetime(1980, 1, 6, 0, 0, 0) # GPS start of time
 
 
 # Convert UTC time to GPS time
@@ -66,20 +89,116 @@ def time_gps2utc(dt):
     time_utm = dt + datetime.timedelta(seconds=-time_diff)
     return time_utm
 
-# Download SP3 file
-def sp3_download(dt, data_folder):
+
+# Compute GPS week, day and time
+def time_gps2wdt(dt=None):
     """
-    sp3_download: Download SP3 file closest to specified date
+    time2gps_wdt: Convert time to gpsWeek, gpsSecWeek, gpsDay, gpsSecsDay
 
     Args:
-        dt: GPS date and time, if missing current time is used
-        data_folder: Location of downloaded file
+        dt: date and time, current time if missing
 
     Returns:
-        res: True on success
+        gpsWeek: GPS week
+        gpsSecWeek: GPS second in week
+        gpsDay: GPS day
+        gpsSecsDay: GPS second in day
     """
 
-    return res
+    if dt == None:
+        dt = datetime.datetime.now()
+    # Convert to GPS time
+    dt_gps = time_utc2gps(dt)
+
+    # Time difference to GPS start
+    gpsDiff = dt_gps - gpsStart
+
+    gpsWeek = int(np.floor(gpsDiff.total_seconds()/secWeek))
+    gpsSecWeek =  gpsDiff.total_seconds() % secWeek
+    gpsDay = int(np.floor(gpsSecWeek/secDay))
+    gpsSecsDay = gpsSecWeek % secDay
+
+    return gpsWeek, gpsSecWeek, gpsDay, gpsSecsDay
+
+
+# Download SP3 file
+def download_sp3(**kwargs):
+    """
+    download_sp3: Download SP3 file closest to specified date
+
+    Args:
+        dt: date and time, if missing current time is used
+        data_folder: Location of downloaded file, if missing current path is used
+
+    Returns:
+        res: downloaded filename, empty on error
+    """
+
+    # Read and set parameters
+    dt = kwargs.get('dt', datetime.datetime.now())
+    data_folder = kwargs.get('data_folder', "")
+    if not os.path.exists(data_folder):
+        print('Data folder %s does not exist, using current folder.' % data_folder)
+        data_folder = ""
+
+    # get GPS time parameters
+    gpsW, _, gpsD, gpsS = time_gps2wdt(dt)
+    gpsH = int(gpsS) // 3600 // 6 * 6
+    # GPS precise orbit filename
+    gps_orbit_fn = "igu%d%d_%02d.sp3.Z" % (gpsW, gpsD, gpsH)
+    # If file not yet available check also previous
+    gpsW2, gpsW2, gpsD2, gpsH2 = gpsW, gpsW, gpsD, gpsH
+    gpsH2 -= 6
+    if gpsH2 < 0:
+        gpsH2 = gpsH2 + 24
+        gpsD2 -= 1
+    if gpsD2 < 0:
+        gpsD2 = gpsD2 + 7
+        gpsW2 -= 1
+    gps_orbit_fn_previous = "igu%d%d_%02d.sp3.Z" % (gpsW2, gpsD2, gpsH2)
+
+    # File to get
+    ftp_file = "%s%d/%s" % (ftp_data_root, gpsW, gps_orbit_fn)
+    get_file = "%s%s" % (data_folder, gps_orbit_fn)
+    # Getting file from FTP
+    # Open FTP connection
+    try:
+        ftp = FTP(ftp_address)
+        ftp.login()
+    except:
+        print("Can not login to %s" % ftp_address)
+        raise
+    # Check if file exists, otherwise load previous
+    try:
+        # Open the file for writing in binary mode
+        ftp.size(ftp_file)
+    except:
+        try:
+            gps_orbit_fn = gps_orbit_fn_previous
+            ftp_file = "%s%d/%s" % (ftp_data_root, gpsW, gps_orbit_fn)
+            get_file = "%s%s" % (data_folder, gps_orbit_fn)
+            ftp.size(ftp_file)
+        except:
+            print("No file %s on server" % gps_orbit_fn)
+            get_file = ""
+    print('Getting precise orbit for ' + gps_orbit_fn)
+    file = open(get_file, 'wb')
+    ftp.retrbinary('RETR %s' % ftp_file, file.write)
+    # Clean up files
+    file.close()
+    ftp.close()
+
+    # Decompress file
+    get_file_dec = get_file[:-2]
+    res = subprocess.call([gunzip, 'e', get_file, "-o" + os.path.dirname(get_file), "-aoa"])
+    if res != 0:
+        print("Can not decopress %" % get_file)
+        print(result.returncode, result.stdout, result.stderr)
+    # Delete downloaded file
+    os.remove(get_file)
+
+    return get_file_dec
+
 
 # Read SP3 file
 def read_sp3(sp3_fn):
@@ -129,7 +248,7 @@ def read_sp3(sp3_fn):
             continue
         if cur_time is None:
             print('NO time found')
-            sys.exit(1)
+            raise
         spf_s.write(str(cur_time) + "," + ",".join(ln.split()[:9]) + "\n")
     spf_s.seek(0)
 
@@ -138,5 +257,52 @@ def read_sp3(sp3_fn):
                          names=df_cols)
     sp3_df["date_time"] = pd.to_datetime(sp3_df["date_time"])
     sp3_df["vehicle"] = sp3_df["vehicle"].map(lambda x: str(x)[1:])
-    sp3_df.set_index('date_time')
+    sp3_df.set_index(["vehicle", 'date_time'], inplace=True)
     return sp3_df
+
+
+# Reproject from ECEF x, y, z to latitude, longitude, altitude
+def reproject_ecef2lla(args):
+    """
+    reproject_ecf2lla: Reproject from ECEF x, y, z to latitude, longitude, altitude as a array
+
+    Args:
+        args are coordinates
+        x: ECEF x coordinate in km
+        y: ECEF y coordinate in km
+        z: ECEF z coordinate in km
+
+    Returns:
+        lat: latitude
+        long: longitude
+        alt: altitude in km
+    """
+
+    # Extract arguments
+    x = args[0] * 1000
+    y = args[1] * 1000
+    z = args[2] * 1000
+
+    lon, lat, alt = pyproj.transform(ecef, lla, x, y, z)
+
+    return pd.Series([lat, lon, alt/1000], index=['lat', 'long', 'alt'])
+
+
+# Convert coordinates from ECEF to lat, long, alt
+def convert_ecef2lla(df, remove=False):
+    """
+    convert_ecef2lla: Convert coordinates in dataframe from ECEF to lat, long, alt
+
+    Args:
+        df: dataframe with coor_x, coor_y, coor_z
+        remove: remove cartesian coordinates, default False
+
+    Returns:
+        df_out: dataframe with lat, lon, alt
+    """
+
+    df_lla = df[["coor_x", "coor_y", "coor_z"]].apply(reproject_ecef2lla, axis=1)
+    df_out = df.join(df_lla)
+    if remove:
+        df_out.drop(["coor_x", "coor_y", "coor_z"], axis=1, inplace=True)
+    return df_out
